9c25b39cdc6a3205227e98920e45915a346468e3
neha <neha@LAPTOP-VLS79GSJ>
2020-05-27 16:32:37 -0500

    This is mine

---
+++ src/main/java/Module1/JUnit.java
@@ -0,0 +1,39 @@
+package Module1;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JUnit {
+	
+	WebDriver d;
+
+	// will use anatation if there is no main method 
+	@Test
+	public void TestJunit () throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
+
+		 d = new ChromeDriver();
+
+		d.get("http://techfios.com/test/billing/?ng=admin/");
+
+		d.manage().window().maximize();
+
+	
+		d.findElement(By.id("username")).sendKeys("techfiosdemo@gmail.com");
+		d.findElement(By.id("password")).sendKeys("abc123");
+
+		
+		d.findElement(By.name("login")).click();
+
+		
+		Thread.sleep(5000);
+
+		d.close();
+		d.quit();
+
+	}
+
+}
